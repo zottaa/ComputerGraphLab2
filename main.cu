981a9ae5e292cf66e2dc0dcfd960645584e1dc92
zottaa <zottaa691@gmail.com>
2022-04-23 06:18:49 +0700

    lab2 complete

--- main.cu
+++ main.cu
@@ -1,96 +1,85 @@
-#include <iostream>
-
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
 
+__global__ void editRows(double *data, int matrixOrder, int rowNumber);
 
-__global__ void determinantGaussGPU(double* data, int matrixOrder, double* result);
-__device__ void matrixToTriangle(double* data, int matrixOrder);
+__host__ double gaussDeterminant (double* data, double* dev_data, int matrixOrder);
 
 int main() {
-    // FILE *f1, *f2;
-    // f1 = fopen("Matrix Orders.txt", "w");
-    // f2 = fopen("Time in milli seconds.txt", "w");
-    int n = 0;
+    FILE *f1, *f2;
 
-    int orders[1] = {3};
-
-    for(n = 0; n < 1; n++){
-        double *data = (double*) malloc(sizeof(double) * (orders[n] * orders[n]));
+    for (int n = 100; n < 3500; n += 20) {
+        double *data = (double *) malloc(sizeof(double) * (n * n));
 
         srand(time(0));
-        for (int i = 0; i < orders[n] * orders[n]; i++){
-            data[i] =(double) (rand() % 11);
-        }
-
-        for (int i = 0; i != orders[n]; i++){
-            for (int j = 0; j != orders[n]; j++)
-                printf("%f ", data[i + orders[n] * j]);
-            printf("\n");
+        for (int i = 0; i < n * n; i++) {
+            data[i] = (double) (rand() % 11);
         }
 
         double determinant = 1;
 
-        printf("matrix order = %d\n", orders[n]);
+        f1 = fopen("Matrix Orders.txt", "a");
+        printf("matrix order = %d\n", n);
+        fprintf(f1, "%d\n", n);
+        fclose(f1);
 
-        double *dev_det;
         double *dev_data;
 
-        cudaMalloc((void**)&dev_data, sizeof(double) * (orders[n] * orders[n]));
-        cudaMemcpy(dev_data, data, sizeof(double) * orders[n] * orders[n], cudaMemcpyHostToDevice);
-
-        cudaMalloc(&dev_det, sizeof(double));
-        cudaMemcpy(dev_det, &determinant, sizeof(double), cudaMemcpyHostToDevice);
-
-        float timeToCalculate = 0.0;
         cudaEvent_t begin, end;
-
         cudaEventCreate(&begin);
         cudaEventCreate(&end);
         cudaEventRecord(begin, 0);
 
-        determinantGaussGPU <<<orders[n], orders[n]>>>(dev_data, orders[n], dev_det);
+        cudaMalloc((void **) &dev_data, sizeof(double) * (n * n));
+        cudaMemcpy(dev_data, data, sizeof(double) * n * n, cudaMemcpyHostToDevice);
+
+        float timeToCalculate = 0.0;
+
+        determinant = gaussDeterminant(data, dev_data, n);
 
         cudaEventRecord(end, 0);
         cudaEventSynchronize(end);
         cudaEventElapsedTime(&timeToCalculate, begin, end);
+
+        f2 = fopen("Time in milli seconds GPU.txt", "a");
+        fprintf(f2, "%.2f\n", timeToCalculate);
+        fclose(f2);
+
         printf("time to calculate = %.2f milliseconds\n", timeToCalculate);
 
-
-        cudaMemcpy(&determinant, dev_det, sizeof(double), cudaMemcpyDeviceToHost);
-
-        printf("determinant %f", determinant);
+        //printf("determinant %f\n", determinant);
 
         cudaEventDestroy(begin);
         cudaEventDestroy(end);
 
-        cudaFree(dev_det);
         cudaFree(dev_data);
         free(data);
     }
-
-
     return 0;
 }
 
-__global__ void determinantGaussGPU(double* data, int matrixOrder, double* result){
-    unsigned int row = blockIdx.x;
-
-    matrixToTriangle(data, matrixOrder);
-
-    *result *= data[row + blockDim.x * row];
+__global__ void editRows(double *data, int matrixOrder, int rowNumber) {
+    __syncthreads();
+    unsigned int idx = blockIdx.x;
+    if (idx > rowNumber && idx <= matrixOrder - 1) {
+        double divider = data[matrixOrder * idx + rowNumber] / data[rowNumber * matrixOrder + rowNumber];
+        for (int j = rowNumber; j < matrixOrder; j++)
+            data[matrixOrder * idx + j] -= data[rowNumber * matrixOrder + j] * divider;
+    }
 }
 
-
-__device__ void matrixToTriangle(double* data, int matrixOrder){
-    unsigned int row = blockIdx.x;
-    unsigned int col = threadIdx.x;
-
-    double divider = 0.0;
-    if (data[row + blockDim.x * row] != 0)
-        divider = data[col + blockDim.x * row] / data[row + blockDim.x * row];
-    for(int k = 0; k < matrixOrder; k++){
-        data[col + blockDim.x * k] -= data[row + blockDim.x] * divider;
+__host__ double gaussDeterminant(double *data, double *dev_data, int matrixOrder) {
+    double determinant = 0;
+    for (int i = 0; i < matrixOrder - 1; i++) {
+        editRows<<<matrixOrder, 1>>>(dev_data, matrixOrder, i);
+        cudaDeviceSynchronize();
     }
+    cudaThreadSynchronize();
+    cudaMemcpy(data, dev_data, matrixOrder * matrixOrder * sizeof(double), cudaMemcpyDeviceToHost);
+    determinant = data[0];
+    for (int i = 1; i < matrixOrder; i++) {
+        determinant *= data[i * matrixOrder + i];
+    }
+    return determinant;
 }
